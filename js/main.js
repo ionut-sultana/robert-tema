1a06620bdb1172c8b96ae5af3b880b6b1ed3cefc
Sultana Ionut <ionut.sultana@littlewebshed.com>
2018-08-13 16:11:38 +0300

    "-Made article body - Made social items - Made more articles - Added fancy box and modals on socials "

---
+++ js/main.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+
+	$("a.group").fancybox({
+		'transitionIn'	:	'elastic',
+		'transitionOut'	:	'elastic',
+		'speedIn'		:	600, 
+		'speedOut'		:	200, 
+		'overlayShow'	:	false
+	});
+	
+});
